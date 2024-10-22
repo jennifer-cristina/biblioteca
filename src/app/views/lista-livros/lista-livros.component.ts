2d95d0272a2df885b31ea3592566d60291a3dd56
Jennifer Cristina <cristin4.jennifer@gmail.com>
2024-10-22 20:11:16 -0300

    feat: busca livros por api da google

---
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { LivroService } from 'src/app/service/livro.service';
+
+@Component({
+  selector: 'app-lista-livros',
+  templateUrl: './lista-livros.component.html',
+  styleUrls: ['./lista-livros.component.css']
+})
+export class ListaLivrosComponent {
+
+  listaLivros: [];
+  campoBusca: string = '';
+
+  constructor(
+    private livroService: LivroService
+  ) { }
+
+  buscarLivros() {
+    this.livroService.buscar(this.campoBusca).subscribe((response) => {
+      console.log(response);
+      
+    });
+  }
+
+}
+
+
+
